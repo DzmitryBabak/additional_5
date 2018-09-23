9ac5815909e08c08c9078956a9d3b40d54566494
Dzmitry Babak <dzmitry.babak@gmail.com>
2018-09-23 22:14:38 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    str.split('').filter((element) => {
+        let isConfigElement = bracketsConfig.find(configElement => configElement[0] === stack[stack.length - 1]);
+        isConfigElement && isConfigElement[1] && isConfigElement[1] === element ? stack.pop() : stack.push(element);
+    });
+    return stack.length === 0;
 }
